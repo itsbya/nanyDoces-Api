3887edb3dafa109bba983c477f1c5940117fc55c
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-05-02 02:03:27 -0300

    :sparkles: Criação produto.controler

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Produto } from "./entities/produto.entity";
 import { Module } from "@nestjs/common";
+import { ProdutoController } from "./controllers/postagem.controller";
+import { ProdutoService } from "./services/produto.service";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Produto])],
-    providers: [],
-    controllers: [],
+    providers: [ProdutoService],
+    controllers: [ProdutoController],
     exports: [],
 })
 export class ProdutoModule {}
