ee521e05d3bb3d128319d6bbf38e55bde0c045fa
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-05-06 01:24:58 -0300

    :wrench: Configuração dos metodos do CRUD na produto.service e produto.controller

--- src/produto/controllers/postagem.controller.ts
+++ src/produto/controllers/postagem.controller.ts
@@ -1,14 +1,55 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { Produto } from "../entities/produto.entity";
 import { ProdutoService } from "../services/produto.service";
 
 @Controller("/produtos")
 export class ProdutoController {
     constructor(private readonly produtoService: ProdutoService) {}
-
+    
+    // Bucar TODOS
     @Get()
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Produto[]> {
         return this.produtoService.findAll();
     }
+    
+    // Bucar por ID
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
+        return this.produtoService.findById(id);
+    }
+
+
+    // Bucar por TITULO
+    @Get('titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    findByTitulo(@Param('titulo') titulo: string): Promise<Produto[]> {
+        return this.produtoService.findByTitulo(titulo);
+    }
+
+
+    // CADASTAR produto
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() produto:Produto): Promise<Produto> {
+        return this.produtoService.create(produto);
+    }
+
+
+    // ATUALIAR produto
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() produto: Produto): Promise<Produto> {
+        return this.produtoService.update(produto);
+    }
+
+
+    // DELETE produto
+    @Delete("/:id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param("id", ParseIntPipe) id: number) {
+        return this.produtoService.delete(id);
+    }
 }
+    
--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Produto } from "../entities/produto.entity";
 
 @Injectable()
@@ -10,7 +10,64 @@ export class ProdutoService {
         private produtoRepository: Repository<Produto>
     ){}
 
+
+    // Bucar TODOS
     async findAll(): Promise<Produto[]> {
         return await this.produtoRepository.find();
     }
+
+
+    // Bucar por ID
+    async findById(id: number): Promise<Produto> {
+
+        const produto = await this.produtoRepository.findOne({
+            where:{
+                id
+            }
+        });
+
+        if(!produto)
+            throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+
+        return produto;
+
+    }
+
+    // Bucar por TITULO
+    async findByTitulo(titulo: string): Promise<Produto[]> {
+        return this.produtoRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}`)
+            }
+        })
+    } 
+
+
+    // CADASTRAR produto
+    async create(produto: Produto): Promise<Produto> {
+        return await this.produtoRepository.save(produto);
+    }
+
+
+    // ATUALIZAR produto
+    async update(produto: Produto): Promise<Produto> {
+       await this.findById(produto.id)
+
+       return await this.produtoRepository.save(produto);
+
+    }
+
+
+    // DELETAR produto
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id)
+
+        return await this.produtoRepository.delete(id)
+    }
+
+
+    
+
+
+
 }
