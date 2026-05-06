c9fdcd5754deb967b06141d7d9be61a41a8cf3e6
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-05-06 01:40:57 -0300

    :sparkles: Criação categoria.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Produto } from './produto/entities/produto.entity';
 import { ProdutoModule } from './produto/produto.module';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
   imports: [
@@ -12,10 +14,11 @@ import { ProdutoModule } from './produto/produto.module';
       username: 'root',
       password: '7803luxO.',
       database: 'db_nanydoces',
-      entities: [Produto],
+      entities: [Produto, Categoria],
       synchronize: true,
     }),
     ProdutoModule,
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
