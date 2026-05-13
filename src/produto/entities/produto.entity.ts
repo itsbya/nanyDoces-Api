89fdee128318f82909c256ec0996479936db1303
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-05-13 00:19:51 -0300

    :ok_hand: Ajuste nos atributos da produto.entity, e implementação de relacionamento com categoria

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,5 +1,8 @@
-import { IsNotEmpty } from "class-validator";
-import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn } from "typeorm";
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUrl } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, ManyToOne } from "typeorm";
+import { NumericTransformer } from "../../../util/numericTranformer";
+import { Categoria } from "../../categoria/entities/categoria.entity";
 
 
 @Entity({name: "tb_produtos"})
@@ -8,14 +11,50 @@ export class Produto {
     @PrimaryGeneratedColumn()
     id: number;
     
+    //REGISTRO DO NOME
+    @Transform(({value} : TransformFnParams) => value ?.trim())
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
-    titulo: string;
+    nome : string;
 
+    
+    //REGISTRO DA DESCRIÇÃO
+    @Transform(({value} : TransformFnParams) => value ?.trim())
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
     descricao: string;
 
-    @UpdateDateColumn()
-    data: Date;
+    
+    //REGISTRO DO PREÇO
+    @IsNumber({maxDecimalPlaces: 2})
+    @IsNotEmpty()
+    @IsPositive()
+    @Column({
+       type: 'decimal',
+       precision: 10,
+       scale: 2,
+       transformer: new NumericTransformer(),
+    })
+    preco: number;
+
+
+    //REGISTRO DO CALORIA
+    @Column({ type: 'int' })
+    caloria: number;
+    
+
+    //REGISTRO DO IMAGEM
+    @IsString()
+    @IsUrl()
+    @Column()
+    imagem: string;
+
+
+
+    //Relacionamento com Categoria
+    @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+        onDelete: "CASCADE"
+    })
+    categoria: Categoria;
+
 }
--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Produto } from "./entities/produto.entity";
 import { Module } from "@nestjs/common";
-import { ProdutoController } from "./controllers/postagem.controller";
+import { ProdutoController } from "./controllers/produto.controller";
 import { ProdutoService } from "./services/produto.service";
 
 
