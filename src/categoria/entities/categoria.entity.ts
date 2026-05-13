9a5b01509e941453e5a3667a522d93d7d727579e
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-05-13 00:22:24 -0300

    :sparkles: Implementação do atributo nome em  categoria.entity, e implementação de relacionamento com produto

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from "class-validator"
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
+import { Produto } from "../../produto/entities/produto.entity"
 
 @Entity({name: "tb_categorias"})
 export class Categoria {
@@ -7,7 +8,13 @@ export class Categoria {
     @PrimaryGeneratedColumn()
     id: number
 
+    @Column({length: 100, nullable: false})
+    nome: string;
+
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
     descricao: string
+
+    @OneToMany(() => Produto, (produto) => produto.categoria)
+    produto: Produto[]
 }
